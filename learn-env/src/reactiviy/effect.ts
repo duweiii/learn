f60f4baed3e2153df153fc8f059646228988687a
duweiii <15106997058@163.com>
2022-06-13 21:53:18 +0800

    add comment about effect & reactive

--- learn-env/src/reactiviy/effect.ts
+++ learn-env/src/reactiviy/effect.ts
@@ -29,6 +29,8 @@ export function trigger(target, key){
   let depsMap = targetMap.get(target)
   let dep = depsMap.get( key );
   for( const effect of dep ){
+    // 这里是为了处理只使用reactive 但不使用effect的情况下
+    // 触发get进行依赖收集时，收集到的是个undefined，因为没有effect配合暴露依赖
     effect && effect.run()
   }
 }
