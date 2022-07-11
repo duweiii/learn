5dcd760fcbb108b0750a190486cff7316dd194fb
duweiii <15106997058@163.com>
2022-07-11 14:19:04 +0800

    learn WebAssembly

---
+++ webAssembly/wasm.js
@@ -0,0 +1,42 @@
+let add;
+let compute;
+let count = 10000;
+
+function fetchAndInitiate(fileName){
+  return fetch(fileName)
+  .then(file => file.arrayBuffer())
+  .then(bits => WebAssembly.compile(bits))
+  .then(module => new WebAssembly.Instance(module))
+}
+
+function jsWhile(count){
+  let index = 0;
+  let result = 0;
+  while(index <= count){
+    result += index;
+    index++;
+  }
+  return result;
+}
+
+function ticker(fn, count){
+  console.time('some')
+  fn(count);
+  console.timeEnd('some')
+}
+
+fetchAndInitiate('test.wasm').then( res =>{
+  add = res.exports._Z3addii;
+  compute = res.exports._Z7computel;
+  console.log( res.exports )
+  window.compute = compute;
+  
+  ticker( compute, count )
+  // 最快 0.0029歪日
+
+  ticker( jsWhile, count);
+  // 最快 0.17ms
+})
+
+
+
